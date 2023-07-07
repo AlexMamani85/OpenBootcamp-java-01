20cea7d6501a63cf61dc1015a948008b62aeef25
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-07-07 15:34:00 -0500

    inicio

---
+++ src/NuevoPaquete/NuevaClase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NuevoPaquete;
+
+/**
+ *
+ * @author alex
+ */
+public class NuevaClase {
+    public static void main(String[] args) {
+        int numero = 5421;
+        long numeroLong = 12;
+        
+        double num = 12.65d;
+        boolean verdadero = true;
+
+        String salud="Buenos dias";
+        
+        System.out.println(numero);
+        System.out.println(numeroLong);
+        System.out.println(num);
+        System.out.println(verdadero);
+        System.out.println(salud);
+                
+    }
+}
