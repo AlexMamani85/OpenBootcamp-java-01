dfb1ab7621dd94213bcdeccf6f85e847a1210ab8
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-07-08 19:08:32 -0500

    Entrega ejercicios tema 5

---
+++ src/antjavaapplication/CocheCRUDImpl.java
@@ -0,0 +1,33 @@
+package antjavaapplication;
+
+
+import antjavaapplication.CocheCRUD;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alex
+ */
+public class CocheCRUDImpl implements CocheCRUD{
+
+    @Override
+    public void save() {
+        System.out.println("save");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("findAll");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("delete");
+    }
+
+
+}
